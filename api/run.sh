b2169ab1b3563d3f85f372d7f2c9658ec1bd45da
Philipp Gille <philipp.gille@gmail.com>
2018-09-14 23:29:46 +0200

    Improve API Dockerfile and run script

--- api/run.sh
+++ api/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set euxo -pipefail
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Starts the container without a public port.
 # The Caddy container can be started with a link to this container and then properly reverse-proxy the requests.
 docker run -d \
     --name ln-ws-api \
     --restart unless-stopped \
-    -v $(pwd)/data/:/root/data/ \
+    -v $SCRIPTDIR/data/:/root/data/ \
     philippgille/ln-ws-api \
     -addr "$LND_ADDR"
     -translateApiKey "$TRANSLATE_API_KEY"
